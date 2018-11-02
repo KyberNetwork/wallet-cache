482cb3d8dc27e45023a7df165d0b2bec7c08e797
NNagato <darkdragon236@gmail.com>
2018-11-02 16:07:51 +0700

    update readme

--- http/server.go
+++ http/server.go
@@ -308,23 +308,23 @@ func (self *HTTPServer) RemoveToken(c *gin.Context) {
 func (self *HTTPServer) Run(kyberENV string) {
 	//self.r.GET("/getRate", self.GetRate)
 	// self.r.GET("/getHistoryOneColumn", self.GetEvent)
-	self.r.GET("/getLatestBlock", self.GetLatestBlock)
+	self.r.GET("/latestBlock", self.GetLatestBlock)
 
-	self.r.GET("/getRateUSD", self.GetRateUSD)
-	self.r.GET("/getRate", self.GetRate)
+	self.r.GET("/rateUSD", self.GetRateUSD)
+	self.r.GET("/rate", self.GetRate)
 	// self.r.GET("/getTokenInfo", self.GetTokenInfo)
 
-	self.r.GET("/getKyberEnabled", self.GetKyberEnabled)
-	self.r.GET("/getMaxGasPrice", self.GetMaxGasPrice)
-	self.r.GET("/getGasPrice", self.GetGasPrice)
-	self.r.GET("/getRightMarketInfo", self.GetRightMarketInfo)
-	self.r.GET("/getLast7D", self.GetLast7D)
-	self.r.GET("/getRateETH", self.GetRateETH)
-	self.r.GET("/getCacheVersion", self.getCacheVersion)
+	self.r.GET("/kyberEnabled", self.GetKyberEnabled)
+	self.r.GET("/maxGasPrice", self.GetMaxGasPrice)
+	self.r.GET("/gasPrice", self.GetGasPrice)
+	self.r.GET("/marketInfo", self.GetRightMarketInfo)
+	self.r.GET("/last7D", self.GetLast7D)
+	self.r.GET("/rateETH", self.GetRateETH)
+	self.r.GET("/cacheVersion", self.getCacheVersion)
 
-	self.r.GET("/coingecko/getRightMarketInfo", self.GetRightMarketInfoCG)
-	self.r.GET("/coingecko/getRateUSD/", self.GetRateUSDCG)
-	self.r.GET("/coingecko/getRateETH", self.GetRateETHCG)
+	self.r.GET("/coingecko/marketInfo", self.GetRightMarketInfoCG)
+	self.r.GET("/coingecko/rateUSD/", self.GetRateUSDCG)
+	self.r.GET("/coingecko/rateETH", self.GetRateETHCG)
 
 	//self.r.GET("/getLanguagePack", self.GetLanguagePack)
 	if kyberENV != "production" {
