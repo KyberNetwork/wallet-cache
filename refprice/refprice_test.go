ab515c84736db68638f60e281c4bc07c74a525e5
Nguyen Le Quang <quangkeu95@gmail.com>
2020-10-08 13:44:10 +0700

    Add kyber reference price

--- refprice/refprice_test.go
+++ refprice/refprice_test.go
@@ -1,18 +1,1 @@
 package refprice
-
-import (
-	"github.com/stretchr/testify/assert"
-	"log"
-	"os"
-	"testing"
-)
-
-func TestGetRefPrice(t *testing.T) {
-	os.Setenv("NODE_ENDPOINT", "https://ethereum.knstats.com/v1/mainnet/geth")
-	ref := NewRefPrice()
-	price, err := ref.GetRefPrice("ETH", "USDT")
-	assert.NoError(t, err)
-	assert.NotEqual(t, 0, price)
-
-	log.Println(price)
-}
